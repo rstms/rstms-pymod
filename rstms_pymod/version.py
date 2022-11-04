d1518e2dd16526811156d6273ff985cec582552d
Matt Krueger <mkrueger@rstms.net>
2022-11-04 09:58:53 -0500

    Bump version: 0.0.1 → 0.0.2

--- rstms_pymod/version.py
+++ rstms_pymod/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-11-03 00:25:49-05:00"
+__timestamp__ = "2022-11-04 09:58:53-05:00"
